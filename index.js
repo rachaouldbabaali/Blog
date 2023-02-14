fdca25ebe7c7fa27b3f3cbae607f544a9430f69e
racha ouldbabaali <rachaould@gmail.com>
2023-02-14 14:56:02 +0100

    final index.html

---
+++ index.js
@@ -0,0 +1,12 @@
+/* random background color*/
+/*
+let hexArray=[1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+let colors=[];
+
+for(i=0;i<6;i++){
+    colors.push(hexArray[Math.floor(Math.random() * hexArray.length)]);
+}
+let finalColor=`#${colors.join("")}`;
+document.body.append(finalColor);
+document.body.style.backgroundColor = finalColor ;
+*/
